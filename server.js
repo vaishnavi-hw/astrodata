e035971623edd5233f9211d3961c6862ed0d4e69
unknown <sumanzzz2024@gmail.com>
2025-05-08 17:57:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const port = 3000;
+
+const app = express();
+app.use(express.urlencoded())
+app.use(express.static(__dirname))
+mongoose.connect('mongodb://127.0.0.1:27017/astrologydata')
+const db =mongoose.connection
+db.once('open',()=>{
+    console.log("Success mongodb")
+})
+const dataSchema = new mongoose.Schema ({
+    lagna:String,
+    bhav:String,
+    grah:String,
+    text:String
+})
+const users = mongoose.model("data",dataSchema)
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+app.post('/post',async(req,res)=>{
+    const {lagna,bhav,grah,text} = req.body;
+    const user = new users({
+        lagna,
+        bhav,
+        grah,
+        text
+    })
+    await user.save()
+    console.log(user)
+})
+app.listen(port,()=>{
+    console.log("server started")
+})
